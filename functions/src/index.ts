ad79145df46880f85669a0dfb30690fda99f6c18
Pavel Karpovich <Chesh397@mail.ru>
2020-08-11 14:07:50 +0300

    Find existing order

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,11 @@
 import * as functions from 'firebase-functions';
 import * as FirebaseAdmin from 'firebase-admin';
 import {google, sheets_v4} from 'googleapis';
-import get from 'lodash/get';
+import cyrillicToTranslit from 'cyrillic-to-translit-js';
+import levenshtein from 'js-levenshtein'
 import chunk from 'lodash/chunk';
+import uniq from 'lodash/uniq';
+import get from 'lodash/get';
 
 const spreadsheetId = '1S-gWGUUqDzfBUUZNRo2UjlHR8kw9LrKQ-I_PvH7nXB4'; // tmp
 
@@ -12,9 +15,15 @@ type StaticTarget = 'kumir' | 'chanakhi';
 
 type ExpectedRequestData = {
     target?: StaticTarget
-    name?: string
+    systemName?: string
+    customName?: string
     items?: string[]
     totalCost?: string
+};
+
+const targetDisplayNames: Record<StaticTarget, string> = {
+    'kumir': 'Ку-Мир',
+    'chanakhi': 'Чанахи',
 };
 
 const getLastFilledRow = async (api: sheets_v4.Sheets) => {
@@ -70,37 +79,86 @@ const getLastFilledRow = async (api: sheets_v4.Sheets) => {
         }
     } while (!foundRow);
 
+    const packSize = 5;
+    let foundCycle2 = false;
     do {
         const {data} = await api.spreadsheets.get({
             spreadsheetId,
             includeGridData: true,
-            ranges: [`B${foundRow}:M${foundRow}`],
+            ranges: [`B${foundRow}:M${foundRow + packSize}`],
         });
-        const rowValues = get(data, 'sheets[0].data[0].rowData[0].values');
-        if (rowValues && rowValues.length) {
-            functions.logger.info(rowValues, {structuredData: true});
-            const filledRowsNumber = rowValues.filter((cell: sheets_v4.Schema$CellData) => cell.userEnteredValue).length;
-            if (filledRowsNumber === 0) {
-                break;
+        const rowData = get(data, 'sheets[0].data[0].rowData');
+
+        for (const {values} of rowData) {
+            if (values && values.length) {
+                functions.logger.info(values, {structuredData: true});
+                const filledRowsNumber = values.filter((cell: sheets_v4.Schema$CellData) => cell.userEnteredValue).length;
+                if (filledRowsNumber === 0) {
+                    foundCycle2 = true;
+                    break;
+                }
             }
         }
-        foundRow++;
-    } while (true);
+        if (!foundCycle2) {
+            foundRow++;
+        }
+    } while (!foundCycle2);
 
     functions.logger.info(`Found Row: ${foundRow}`, {structuredData: true});
     return foundRow;
 };
 
-const targetDisplayNames: Record<StaticTarget, string> = {
-    'kumir': 'Ку-Мир',
-    'chanakhi': 'Чанахи',
+const checkExistedOrder = async (api: sheets_v4.Sheets, names: string[], rowNumber: number) => {
+    const {data} = await api.spreadsheets.get({
+        spreadsheetId,
+        includeGridData: true,
+        ranges: [`D${rowNumber - 30}:D${rowNumber}`],
+    });
+    const rowData = get(data, 'sheets[0].data[0].rowData');
+    const existedNames = [];
+    let emptyCellSpan = 0;
+    for (let i = rowData.length - 1; i >= 0; i--) {
+        const value = rowData[i].values[0];
+        if (!value || !value.userEnteredValue || !value.userEnteredValue.stringValue) {
+            if (existedNames.length) {
+                emptyCellSpan++;
+                if (emptyCellSpan === 2) {
+                    break;
+                }
+            }
+        } else {
+            existedNames.push(value.userEnteredValue.stringValue);
+            emptyCellSpan = 0;
+        }
+    }
+    functions.logger.info(existedNames, {structuredData: true});
+    
+    const translit = new cyrillicToTranslit();
+    const nameVariants = uniq(names.reduce<string[]>((total, next) => {
+        const lName = next.toLowerCase().trim();
+        total.push(translit.transform(lName));
+        total.push(translit.reverse(lName));
+        const splits = lName.split(' ').filter(namePart => namePart);
+        if (splits.length > 1) {
+            total.push(`${splits[1]} ${splits[0]}`);
+        }
+        total.push(...splits);
+        return total;
+    }, []));
+    functions.logger.info(nameVariants, {structuredData: true});
+    const foundName = existedNames
+    .map(existedName => existedName.toLowerCase().trim())
+    .find(existedName => !!nameVariants.find(name => levenshtein(existedName, name) < 3));
+
+    functions.logger.info(`Found name: ${foundName}`, {structuredData: true});
+    return !!foundName;
 };
 
 const writeOrderToRow = async (api: sheets_v4.Sheets, order: ExpectedRequestData, rowNumber: number) => {
 
-    if (order.name && order.items && order.items.length) {
+    if (order.systemName && order.items && order.items.length) {
         const targetName = order.target &&(targetDisplayNames[order.target] || order.target);
-        const name = order.name;
+        const name = order.customName ? order.customName : order.systemName;
         const orderItemGroups = chunk(order.items, 6);
         while (orderItemGroups[orderItemGroups.length - 1].length < 6) {
             orderItemGroups[orderItemGroups.length - 1].push('');
@@ -155,8 +213,19 @@ export const placeOrders = functions.https.onRequest(async (request, response) =
         const auth = await google.auth.getClient({ scopes: ['https://www.googleapis.com/auth/spreadsheets'] });
         const api = google.sheets({ version: 'v4', auth });
         const lastRow = await getLastFilledRow(api);
-        await writeOrderToRow(api, data, lastRow);
-        response.status(200).send(`Row: ${lastRow}`);
+        const isOrderAlreadyThere = await checkExistedOrder(api, [data.systemName || '', data.customName || ''], lastRow);
+        if (!isOrderAlreadyThere) {
+            await writeOrderToRow(api, data, lastRow);
+            response.status(200).send({
+                success: true,
+                row: lastRow,
+            });
+        } else {
+            response.status(200).send({
+                success: false,
+                reason: 'Your order already exists',
+            });
+        }
     } catch (err) {
         functions.logger.info(`ERROR: ${err}`, { structuredData: true });
         response.status(500).send({ err });
