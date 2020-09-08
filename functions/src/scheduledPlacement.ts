ed4e25927b715df8682f0dfe61a03f00d35efdf4
Pavel <Chesh397@mail.ru>
2020-09-08 23:39:55 +0300

    Do scheduled order placement in sequence, one by one, to fix concurrency bugs

--- functions/src/scheduledPlacement.ts
+++ functions/src/scheduledPlacement.ts
@@ -14,15 +14,33 @@ export const scheduledPlacement = async () => {
     const randomModeTarget = 'kumir';
     const menu = await getUpdatedMenu(randomModeTarget);
     const tableData = await FirebaseAdmin.firestore().collection('auto-order-configs').get();
-    tableData.forEach(async entry => {
-        const data = entry.data() as ConfigState;
-        if (data.enabled) {
-            functions.logger.info(`Start order placement for ${data.customName || data.systemName}...`);
-            let result: number | null = null;
-            if (data.mode === 'random' && menu) {
-                try {
-                    const items = await randomizeItems(randomModeTarget, menu, defaultRandomOrderConfig);
-                    if (items) {
+    const operations: Array<() => Promise<void>> = []
+    tableData.forEach(entry => {
+        operations.push(async () => {
+            const data = entry.data() as ConfigState;
+            if (data.enabled) {
+                functions.logger.info(`Start order placement for ${data.customName || data.systemName}...`);
+                let result: number | null = null;
+                if (data.mode === 'random' && menu) {
+                    try {
+                        const items = await randomizeItems(randomModeTarget, menu, defaultRandomOrderConfig);
+                        if (items) {
+                            result = await placeOrder({
+                                spreadsheetId: data.spreadsheetId,
+                                systemName: data.systemName,
+                                customName: data.customName,
+                                targets: data.savedTargets,
+                                overwrite: data.overwriteAlways,
+                                allowMultiple: data.allowMultipleOrders,
+                                items,
+                            });
+                        }
+                    } catch { }
+                }
+                if (data.mode === 'preset' || !result) {
+                    try {
+                        const presetId = sample(data.selectedPresets);
+                        const chosenPreset = data.presets.find(preset => preset.id === presetId)!;
                         result = await placeOrder({
                             spreadsheetId: data.spreadsheetId,
                             systemName: data.systemName,
@@ -30,29 +48,18 @@ export const scheduledPlacement = async () => {
                             targets: data.savedTargets,
                             overwrite: data.overwriteAlways,
                             allowMultiple: data.allowMultipleOrders,
-                            items,
+                            items: chosenPreset.items,
                         });
-                    }
-                } catch { }
+                    } catch { }
+                }
+                functions.logger.info(result ? `Order was placed on the row ${result}` : 'Placement fails');
+            } else {
+                functions.logger.info(`Skip order placement for ${data.customName || data.systemName}...`);
             }
-            if (data.mode === 'preset' || !result) {
-                try {
-                    const presetId = sample(data.selectedPresets);
-                    const chosenPreset = data.presets.find(preset => preset.id === presetId)!;
-                    result = await placeOrder({
-                        spreadsheetId: data.spreadsheetId,
-                        systemName: data.systemName,
-                        customName: data.customName,
-                        targets: data.savedTargets,
-                        overwrite: data.overwriteAlways,
-                        allowMultiple: data.allowMultipleOrders,
-                        items: chosenPreset.items,
-                    });
-                } catch { }
-            }
-            functions.logger.info(result ? `Order was placed on the row ${result}` : 'Placement fails');
-        } else {
-            functions.logger.info(`Skip order placement for ${data.customName || data.systemName}...`);
-        }
+        });
     });
+
+    await operations.reduce((chain: Promise<void>, next: () => Promise<void>) => {
+        return chain.then(next)
+    }, Promise.resolve())
 };
