a297e27164b72fe58d5e9de8725994ede3d56843
Pavel Karpovich <Chesh397@mail.ru>
2020-09-22 02:51:54 +0300

    Fix 'getLastFilledRow' algorithm another timer

--- functions/src/placeOrder.ts
+++ functions/src/placeOrder.ts
@@ -24,35 +24,47 @@ const getLastFilledRow = async (api: sheets_v4.Sheets, spreadsheetId: string): P
         const sheetData = await api.spreadsheets.get({ spreadsheetId });
         const maxRow = get(sheetData, 'data.sheets[0].properties.gridProperties.rowCount');
         console.log('@maxRow: ', maxRow);
-        const range = `D${maxRow - bulkSize}:D${maxRow}`;
-        const {data} = await api.spreadsheets.get({
-            spreadsheetId,
-            includeGridData: true,
-            ranges: [range],
-        });
-        if (data.sheets && data.sheets.length) {
-            if (data.sheets[0].data && data.sheets[0].data.length) {
-                const rowData = data.sheets[0].data[0].rowData;
-                if (rowData) {
-                    let value = null;
-                    let i = rowData.length - 1;
-                    do {
-                        if (rowData[i] && rowData[i].values && rowData[i].values![0]) {
-                            value = rowData[i].values![0].userEnteredValue;
+        let fromRow, iter = 0;
+        do {
+            fromRow = maxRow - (bulkSize * (iter + 1));
+            fromRow = fromRow < 1 ? 1 : fromRow;
+            const toRow = maxRow - (bulkSize * iter);
+            const range = `D${fromRow}:D${toRow}`;
+            console.log('Range: ', range);
+            const {data} = await api.spreadsheets.get({
+                spreadsheetId,
+                includeGridData: true,
+                ranges: [range],
+            });
+            if (data.sheets && data.sheets.length) {
+                if (data.sheets[0].data && data.sheets[0].data.length) {
+                    const rowData = data.sheets[0].data[0].rowData;
+                    if (rowData) {
+                        let value = null;
+                        let i = rowData.length - 1;
+                        do {
+                            if (rowData[i] && rowData[i].values && rowData[i].values![0]) {
+                                value = rowData[i].values![0].userEnteredValue;
+                                if (value) {
+                                    foundRow = fromRow + i + 1;
+                                    break;
+                                }
+                            }
+                            --i;
+                        } while (i >= 0);
+                        if (!value) {
+                            functions.logger.warn('Unable to locale last filled row, or spreadsheet is empty, for range ' + range);
                         }
-                        --i;
-                    } while (!value && i >=0);
-                    foundRow = maxRow - bulkSize + i + 2;
-                    if (!value) {
-                        functions.logger.warn('Unable to locale last filled row, or spreadsheet is empty, for range ' + range);
                     }
+                } else {
+                    throwError('not-found', `Cannot retreive spreadsheet ${spreadsheetId} range data ${range}`);
                 }
             } else {
-                throwError('not-found', `Cannot retreive spreadsheet ${spreadsheetId} range data ${range}`);
+                throwError('not-found', `Cannot find spreadsheet pages in the document with id ${spreadsheetId}`);
             }
-        } else {
-            throwError('not-found', `Cannot find spreadsheet pages in the document with id ${spreadsheetId}`);
-        }
+            iter++;
+        } while (fromRow !== 1 && !foundRow);
+
         console.log('@founRow iter1: ', foundRow);
         const packSize = 5;
         let rowShift = 0;
@@ -68,7 +80,6 @@ const getLastFilledRow = async (api: sheets_v4.Sheets, spreadsheetId: string): P
 
             for (let i = rowData.length - 1; i >= 0; --i) {
                 if (rowData[i].values && rowData[i].values.length) {
-                    functions.logger.info(rowData[i].values);
                     const filledRowsNumber = rowData[i].values.filter((cell: sheets_v4.Schema$CellData) => cell.userEnteredValue).length;
                     if (filledRowsNumber !== 0) {
                         rowShift = i + 1;
